1597bcab1b21dc410d4af9610cd600adc3565545
Debanjan Amin <officialdebanjanamin2004@gmail.com>
2024-12-23 21:55:56 +0530

    Delete add_1_number_on_every_digit.py

--- add_1_number_on_every_digit.py
+++
@@ -1,9 +0,0 @@
-number = input("Enter a five-digit number: ")
-
-new_number = ""
-
-for digit in number:
-    new_digit = str((int(digit) + 1) % 10)  
-    new_number += new_digit
-
-print(f"The new number is: {new_number}")
